d8f918c95064b08b05a304a58d837d2a4c94ea48
marcela-revelo <revelomarcela8@gmail.com>
2023-11-16 20:29:12 -0600

    agrege el codigo de la camara

--- lib/pages/foto.dart
+++ lib/pages/foto.dart
@@ -1,7 +1,8 @@
+import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'foto.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:saver/pages/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,9 +19,44 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.deepOrange,
         colorScheme: ColorScheme.dark(),
       ),
-      home: const FotoPage(title: 'Camara'),
+      home: const MyHomePage(title: 'Camara'),
       debugShowCheckedModeBanner: false,
       debugShowMaterialGrid: false,
     );
   }
 }
+//este codigo es de la camara
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> paths = [];
+  XFile? currentImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          widget.title,
+          style: const TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            currentImage != null
+                ? Image(image: FileImage(File(currentImage!.path)))
+                : Icon(Icons.camera_alt, size: 100.0, color: Colors.grey),
+            ElevatedButton(
+              onPressed: () async {
+                final ImagePicker picker = ImagePicker();
+                final XFile? image =
+                    await picker.pickImage(source: ImageSource.camera);
+
+                if (image != null) {
+                  setState(() {
+                    paths.add(image.path);
+                    currentImage = image;
+                  });
+                }
+              },
